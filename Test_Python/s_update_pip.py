791b2f3b774387863273bf9245317426d2c74b7f
vanshin-ACER <kfx721@hotmail.com>
2017-03-22 14:38:32 +0800

    noth

--- Test_Python/s_update_pip.py
+++ Test_Python/s_update_pip.py
@@ -45,13 +45,20 @@ def main():
     b_path = "Lib\\site-packages\\pip\\models\\"
     full_path = os.path.join(pre_path, b_path)
     index_file = os.path.join(full_path, index)
-    with open(index_file, "r") as f:
+    with open(index_file, "r+") as f:
+        # 读取文件的所有内容
         lines = f.readlines()
-        
-        
-        
-    print(full_path)
-    a = "123"
-    a.replace
+        # 将文件的偏移量设置为开始
+        f.seek(0)
+        # 将文件的内容全部删除
+        f.truncate()
+        d = ''
+        for line in lines:
+            # 将需要更改的地方更改
+            d = d+line.replace("python.org", "douban.com")
+        # 将修改后的文件输入
+        f.write(d)
+
+
 if __name__ == '__main__':
     main()
